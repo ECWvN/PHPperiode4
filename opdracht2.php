2ff33a6c6322223318b7df1ee59fa113be041f19
Emma van Nieuwburg <535375@student.glu.nl>
2020-07-04 22:52:10 +0200

    opdracht2

--- opdracht2.php
+++ opdracht2.php
@@ -1,1 +1,50 @@
-//opdracht2
+<!-- opdracht2
+OPDRACHT
+Schrijf zelf een class voor een zelfbedacht object
+
+VOORBEELD: voor het object  Person zou je hetvolgende kunnnen bedenken:
+
+class: Person
+properties: voornaam, tussenvoegsel, achternaam en leeftijd
+
+Bedenk daar een aantal methods bij.
+Voorbeeld van methods voor Person:
+ 
+
+getFullname()
+setFullname()
+
+setAge()
+getAge()
+
+ 
+
+Lever je class in via een github of gitlab - repository
+
+######## extra uitdaging:
+
+probeer of je een superclass met subclasses kunt maken van je objecten, bijvoorbeeld
+superclass is 'huisdier', met algemene kenmerken, zoals aantal poten,
+subclasses zijn bijvoorbeeld  'hond' of 'vis', met hun eigen kenmerken.
+De subclass hond zou een functie kunnen hebben als:
+
+function bark() { return "woof"; } -->
+
+<?php
+class Guitar{
+    // the properties
+
+    public $name;
+    function set_name($name){
+        $this->name=$name;
+    }
+
+}
+$fenderT = new Guitar();
+$fenderT->name="Fender Telecaster";
+
+
+
+
+
+?>
