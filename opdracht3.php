e603cb2d7458699017687badd0d25d232a53a09f
Emma van Nieuwburg <535375@student.glu.nl>
2020-07-04 23:12:31 +0200

    opdracht

---
+++ opdracht3.php
@@ -0,0 +1,35 @@
+//opdracht 3
+
+<?php
+class Guitar {
+  public $name;
+  public $color;
+
+  function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color;
+  }
+  function get_name() {
+    return $this->name;
+  }
+  function get_color() {
+    return $this->color;
+  }
+  function get_wood() {
+    return $this->color;
+  }
+  function get_age() {
+    return $this->color;
+  }
+
+}
+
+$fenderT = new Guitar("fender telecaster","red","maple","1978");
+echo $fenderT->get_name();
+echo "<br>";
+echo $fenderT->get_color();
+echo "<br>";
+echo $fenderT->get_wood();
+echo "<br>";
+echo $fenderT->get_age();
+?>
