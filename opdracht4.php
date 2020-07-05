3a640196e6e24fd2b259bcfcb84ef5bb76bcc40d
Emma van Nieuwburg <535375@student.glu.nl>
2020-07-05 19:59:54 +0200

    opdracht4

--- opdracht4.php
+++ opdracht4.php
@@ -6,8 +6,23 @@
      public function__construct($name){
          $this->name =$name;
      }
-     abstract public function intro() : string;
+     abstract public function games() : string;
  }
- 
+ class fps extends game {
+	public function games() : string {
+		 $this -> name;
+    }
+    class storyline extends game {
+        public function games() : string {
+             $this -> name;
+        }
+ }
+$fps = new fps('Overwatch');
+echo $fps -> games();
+echo "<br>";
+
+$storyline = new storyline('overwatch2');
+echo $storyline -> games();
+echo "<br>";
  
  ?>
