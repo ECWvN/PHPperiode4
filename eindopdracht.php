14f7d0157010d577fab91a5d22bd416c80880dd4
Emma van Nieuwburg <535375@student.glu.nl>
2020-07-09 22:42:51 +0200

    dfg

--- eindopdracht.php
+++ eindopdracht.php
@@ -1,3 +1,25 @@
 <?php
+// class van figuur met de properties//
 
-?>
+class figuur {
+    public $PI = 3.1415;
+    protected $x;
+    protected $y;
+    private $omschrijving;
+
+    public function __construct($Ix,$Iy){
+
+    }
+}
+
+class Cilinder extends figuur {
+      
+    public function __construct($Ih,$Ir){
+        
+
+    }
+    public function getH(){
+        
+    }
+    
+}
